fafef028e918273d25301b77141732c15e5d6266
BlackRing8 <gprakoso123@gmail.com>
2025-12-18 19:50:40 +0700

    downgrade next version for compatible vercel deployment

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "@/components/providers";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata = {
   title: "SKRIPSI APP",
@@ -20,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-sky-100`}>
+      <body className={`antialiased bg-sky-100`}>
         <Providers>{children}</Providers>
       </body>
     </html>
